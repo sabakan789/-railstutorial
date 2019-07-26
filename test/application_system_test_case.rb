fb0c6c00ea85d281b20db73447773688c2d866a5
”Nanayaku” <sabanomisoni789@gmail.com>
2019-07-26 15:35:54 +0900

    Implement basic login

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,8 +1,5 @@
-require 'test_helper'
+require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  test "full title helper" do
-    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
-  end
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
