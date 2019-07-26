5866912183302ab3b34772725aee72a15f2236dd
”Nanayaku” <sabanomisoni789@gmail.com>
2019-07-26 20:45:52 +0900

    login first

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-  else
-    flash.now[:danger] = 'Invalid email/password combination'
-    render 'new'
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
-end
 
   def destroy
   end
