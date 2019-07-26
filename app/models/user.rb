82dfe735d5e8ada39e8f9377179a42f6a60575cc
”Nanayaku” <sabanomisoni789@gmail.com>
2019-07-26 20:07:53 +0900

    session g

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,10 @@
 class User < ApplicationRecord
-attr_accessor :remember_token
-
-  before_save {email.downcase!}
-  validates :name,  presence: true, length: { maximum:  50 }
+  before_save { email.downcase! }
+  validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true,
-    length: { minimum: 6 }, allow_nil: true
-
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  test "full title helper" do
+    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+  end
 end
